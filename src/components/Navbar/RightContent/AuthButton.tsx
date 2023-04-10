9b5958adcf196368ef25864b0aaae57d99ceaa1c
rixyo <saleh35-3063@diu.edu.bd>
2023-04-11 02:12:01 +0600

    Button Changed

--- src/components/Navbar/RightContent/AuthButton.tsx
+++ src/components/Navbar/RightContent/AuthButton.tsx
@@ -1,6 +1,6 @@
 
 
-import { Button } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from '@/atoms/authModalAtom';
@@ -13,18 +13,23 @@ const AuthButtons: React.FC = () => {
 
   return (
     <>
+    <Flex    onClick={() => setAuthModalState({ open: true, view: "login" })}>
+
       <Button
-        color= "blue.500"
+        
         border= "1px solid"
         borderColor= "teal.500"
         height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
-        onClick={() => setAuthModalState({ open: true, view: "login" })}
+     
       >
         Log In
       </Button>
+    </Flex>
+    <Flex    onClick={() => setAuthModalState({ open: true, view: "signup" })}>
+
       <Button
      color= "white"
      bg= "teal.500"
@@ -34,10 +39,11 @@ const AuthButtons: React.FC = () => {
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
-        onClick={() => setAuthModalState({ open: true, view: "signup" })}
+     
       >
         Sign Up
       </Button>
+    </Flex>
     </>
   );
 };
